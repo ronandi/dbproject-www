c632e4ccca3850ee9f800bb9f7d5dc051aefbc34
Rohit Kumar <rohit.kumar@rutgers.edu>
2013-11-23 19:10:20 -0500

    table views

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'sequel'
 gem 'mysql2'
+gem 'will_paginate'
+gem 'will_paginate-bootstrap'
 
 ruby '2.0.0'
--- app.rb
+++ app.rb
@@ -1,6 +1,40 @@
 require 'sinatra'
 require 'sequel'
+require 'will_paginate'
+require 'will_paginate/sequel'
+require 'will_paginate-bootstrap'
+
+user = ENV['cs336user'] || "Your user here"
+pass = ENV['cs336pass'] || "Your pass here"
+DB = Sequel.connect("mysql2://#{user}:#{pass}@cs336-24.cs.rutgers.edu/project").extension(:pagination)
+
+beers = DB[:beers]
+drinkers = DB[:drinkers]
+favorites = DB[:favorite]
 
 get '/' do
   erb :index
+end
+
+get '/beers' do
+  @beers = beers.all
+  erb :beers
+end
+
+get '/drinkers' do
+  page_num = params[:page] || 1
+  @drinkers = drinkers.paginate(page_num.to_i, 100)
+  erb :drinkers
+end
+
+get '/favorites' do
+  page_num = params[:page] || 1
+  @favorites = DB.fetch('select name, beer from drinkers join favorite where id = drinker_id').paginate(page_num.to_i, 100)
+  erb :favorites
+end
+
+get '/likes' do
+  page_num = params[:page] || 1
+  @likes = DB.fetch('select name, beer from drinkers join likes where id = drinker_id').paginate(page_num.to_i, 100)
+  erb :likes
 end
